5ed23f849122d813cdacc04d98f2c01f51b86c16
Tzuyu <fcebk17@gmail.com>
2024-04-26 12:23:22 +0800

    feat: init spring boot application

---
+++ src/test/java/onlyfun/caterpillar/SpringBootAppTest.java
@@ -0,0 +1,13 @@
+package onlyfun.caterpillar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
