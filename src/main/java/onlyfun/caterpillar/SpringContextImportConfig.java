4a8afecac95329ca31ddbd54e62e95338adc47cb
Tzuyu <fcebk17@gmail.com>
2024-04-26 12:37:42 +0800

    fix: del contextconfig

--- src/main/java/onlyfun/caterpillar/SpringContextImportConfig.java
+++
@@ -1,9 +0,0 @@
-package onlyfun.caterpillar;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource({"classpath:spring-config.xml"})
-public class SpringContextImportConfig {
-}
