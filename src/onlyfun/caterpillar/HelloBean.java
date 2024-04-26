d6143a13fb8e312593339c990429e26fc20e94dd
Tzuyu <fcebk17@gmail.com>
2024-04-26 09:49:11 +0800

    Initial commit

---
+++ src/onlyfun/caterpillar/HelloBean.java
@@ -0,0 +1,13 @@
+package onlyfun.caterpillar;
+
+public class HelloBean {
+    private String helloWorld;
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+}
---
+++ src/onlyfun/caterpillar/SpringDemo.java
@@ -0,0 +1,19 @@
+package onlyfun.caterpillar;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class SpringDemo {
+    public static void main(String[] args) {
+//        Resource rs = new FileSystemResource("spring-config.xml");
+//        BeanFactory factory = new XmlBeanFactory(rs);
+        ApplicationContext context = new FileSystemXmlApplicationContext("src/spring-config.xml");
+
+        HelloBean hellobean = (HelloBean) context.getBean("helloBean");
+        System.out.println(hellobean.getHelloWorld());
+    }
+}
