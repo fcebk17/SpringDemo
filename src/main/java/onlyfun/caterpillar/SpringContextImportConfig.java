0503c86bb84cf3de4a95e0c7b0a53f11b47224ce
Tzuyu <fcebk17@gmail.com>
2024-04-26 12:38:40 +0800

    feat: SpringContextImportConfig.java

---
+++ src/main/java/onlyfun/caterpillar/SpringContextImportConfig.java
@@ -0,0 +1,9 @@
+package onlyfun.caterpillar;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({"classpath:spring-config.xml"})
+public class SpringContextImportConfig {
+}
